ae320cd739d63abee8e198b491e32482eb38b768
Dayoming <jdayoung1204@naver.com>
2024-06-03 23:04:33 +0900

    Initial commit

---
+++ src/main/java/com/learntogether/learn_together/LearnTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.learntogether.learn_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntogether/learn_together/LearnTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntogether.learn_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
