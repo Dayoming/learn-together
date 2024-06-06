9428b019bd3bac451cae2cf947aa3e604d71ed48
Dayoming <jdayoung1204@naver.com>
2024-06-06 23:18:26 +0900

    Feat: IndexController를 추가하여 root URL mapping 처리

---
+++ src/main/java/com/learntogether/learn_together/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.learntogether.learn_together.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
